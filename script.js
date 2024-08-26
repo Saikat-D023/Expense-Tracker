6bf6959abe30e41ff3d3da54c06d0e7e0062a780
Saikat-D023 <saikatdey358@gmail.com>
2024-08-26 11:15:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const expenseForm = document.getElementById("expense-form");
+        const expenseList = document.getElementById("expenseList");
+
+        expenseForm.addEventListener('submit',function(event){
+            event.preventDefault()
+
+            const description = document.getElementById("description").value;
+            const category = document.getElementById("category").value;
+            const amount = document.getElementById("amount").value;
+
+            
+                const newRow = document.createElement('tr')
+
+                newRow.innerHTML = `<td>${description}</td>
+                                    <td>${category}</td>
+                                    <td>${amount}</td>`;
+                
+                expenseList.appendChild(newRow)
+
+            document.getElementById('description').value=''
+            document.getElementById('category').value=''
+            document.getElementById('amount').value=''
+            
+
+        })
